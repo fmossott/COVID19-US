582664cdb0c485068df88d703a6eb24647d30af2
fmossott <franco.mossotto@hcl.com>
2020-03-17 14:12:11 +0100

    newlines missing

--- merge.sh
+++ merge.sh
@@ -4,6 +4,7 @@ here=`dirname $0`
 src=$here/../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports
 
 tgt=$here/data/cases.csv
+wrk=$tgt.wrk
 
 tgtdir=`dirname $tgt`
 
@@ -13,11 +14,14 @@ fi
 
 first=true
 for f in $src/*.csv; do
-  echo adding $f to $tgt
+  echo adding $f to $wrk
   if $first ; then
-    head -n1 $f > $tgt
+    head -n1 $f > $wrk
     first=false
   fi
-  tail -n+2 $f >> $tgt
+  tail -n+2 $f >> $wrk
+  echo >> $wrk
 done
+
+sed '/^$/d' $wrk > $tgt
 
