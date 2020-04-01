8b47de3c43e3d2709aca6add60ff11ec0642e0c1
fmossott <franco.mossotto@hcl.com>
2020-04-01 08:20:41 +0200

    update merge script

--- combine.sh
+++ combine.sh
@@ -3,3 +3,4 @@
 cd `dirname $0`
 python3 pyscript/mergeTimeSeries.py -s ../COVID-19/csse_covid_19_data/csse_covid_19_time_series/ -o data/ts_raw.csv
 python3 pyscript/mergeDailyReports.py -s ../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/ -o data/
+python3 pyscript/mergeWorldData.py -s COVID-19/csse_covid_19_data/csse_covid_19_time_series/
